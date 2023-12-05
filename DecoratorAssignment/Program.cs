c136aa25f42ff565e66931e38d2975948ca146d8
Boris <146715808+XeUby@users.noreply.github.com>
2023-12-05 02:03:46 +0200

    Update Program.cs

--- DecoratorAssignment/Program.cs
+++ DecoratorAssignment/Program.cs
@@ -1,17 +1,31 @@
-﻿namespace DecoratorAssignment
+using DecoratorAssignment.BeveragesList;
+using DecoratorAssignment.CondimentsList;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DecoratorAssignment
 {
-    // Instructions
-    // You can implement your whole solution here
-    // Optionally you can use folder structure if you deem it necessary
-    // For this Assignment we will use Decorator pattern example introduced in the book Head First Design Patterns by O'Reilly
-    // Chapeter 3 the DecoratorPattern: Decorating Objects (starts at page 79)
-    // Link to pdf: https://github.com/ajitpal/BookBank/blob/master/%5BO%60Reilly.%20Head%20First%5D%20-%20Head%20First%20Design%20Patterns%20-%20%5BFreeman%5D.pdf
-    // NOTE: Remember that the code examples in this book are written in java so you can't just copy the code examples given there
+  
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            // Example 1: Espresso
+            Beverage beverage = new Espresso();
+            Console.WriteLine(beverage.GetDescription() + " $" + beverage.Cost());
+
+            // Example 2: DarkRoast with Mocha and Whip
+            Beverage beverage2 = new DarkRoast();
+            beverage2 = new Mocha(beverage2);
+            beverage2 = new Mocha(beverage2);
+            beverage2 = new Whip(beverage2);
+            Console.WriteLine(beverage2.GetDescription() + " $" + beverage2.Cost());
+
+            // Example 3: HouseBlend with Soy, Mocha, and Whip
+            Beverage beverage3 = new HouseBlend();
+            beverage3 = new Soy(beverage3);
+            beverage3 = new Mocha(beverage3);
+            beverage3 = new Whip(beverage3);
+            Console.WriteLine(beverage3.GetDescription() + " $" + beverage3.Cost());
         }
     }
 }
